9973a27cfc5322dfcd092881d7265f60f6941b56
jinhyuk <jjhyuk15@gmail.com>
2021-07-09 21:53:58 +0900

    Initial Commit

---
+++ typicode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  typicode
+//
+//  Created by jinhyuk on 2021/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
